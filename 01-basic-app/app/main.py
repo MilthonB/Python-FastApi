e952bbc8d823a9ba3a978e9dfa6f8479ac0672f4
MB <milthonbor@gmail.com>
2021-09-26 19:45:42 -0700

    peticion put y interface estilo js

--- 01-basic-app/app/main.py
+++ 01-basic-app/app/main.py
@@ -1,8 +1,19 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 from typing import Optional
 
-app = FastAPI()
+app = FastAPI() #Objeto creado de fastapi que se ejecuta en uvicorn
 
+
+
+#Aprender a manejar los estados de las respuesta de peticiones   
+#Mandar informacion por heades, querys, paramas,
+
+
+class Item(BaseModel): # iterface de peticion de un put  elementos que debe de contener el put para pasar 
+    name: str
+    price: float
+    is_offer: Optional[bool] = None
 
 @app.get("/")
 def read_root():
@@ -12,3 +23,11 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
+
+@app.put("/items/{item_id}")
+def update_item(item_id: int, item: Item):
+    return {"item_name": item.price, "item_id": item_id}
+
+
+
+
