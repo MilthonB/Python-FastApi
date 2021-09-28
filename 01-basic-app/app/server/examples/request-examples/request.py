21f7122339ac785c83f3acc6738ded81307c4334
MB <milthonbor@gmail.com>
2021-09-27 22:09:10 -0700

    Field argumentos adicionales

--- 01-basic-app/app/server/examples/request-examples/request.py
+++ 01-basic-app/app/server/examples/request-examples/request.py
@@ -1,7 +1,7 @@
 from typing import Optional
 
 from fastapi import FastAPI
-from pydantic import BaseModel
+from pydantic import BaseModel,  Field
 
 app = FastAPI()
     
@@ -27,6 +27,23 @@ class Item(BaseModel):
                 "tax": 3.2,
             }
         }
+
+
+#usando Field puede agregar o añadir example para cada campo 
+# esto se mostrara en localhos docs
+# los datos se muestra igual
+class Item2(BaseModel):
+    name: str = Field(..., example="Foo")
+    description: Optional[str] = Field(None, example="A very nice Item")
+    price: float = Field(..., example=35.4)
+    tax: Optional[float] = Field(None, example=3.2)
+
+
+@app.put("/put/example/items/{item_id}")
+async def update_item(item_id: int, item: Item):
+    results = {"item_id": item_id, "item": item}
+    return results
+
 
 
 @app.put("/schema/items/{item_id}")
