0c80e01178be9538895c814711204a5d478d1da0
MB <milthonbor@gmail.com>
2021-09-27 22:17:35 -0700

    reques examples

--- 01-basic-app/app/server/examples/request-examples/request.py
+++ 01-basic-app/app/server/examples/request-examples/request.py
@@ -54,17 +54,47 @@ async def update_item(item_id: int, item: Item):
     return results
 
 
+#Body con múltiples examples
+# Anidados unos con otros 
+#  Esto se omuestra en la OpenAPI
 @app.put("/put/body/items/{item_id}")
 async def update_item(
     item_id: int,
     item: Item = Body(
         ...,
-        example={
-            "name": "Foo",
-            "description": "A very nice Item",
-            "price": 35.4,
-            "tax": 3.2,
+        examples={
+            "normal": {
+                "summary": "A normal example",
+                "description": "A **normal** item works correctly.",
+                "value": {
+                    "name": "Foo",
+                    "description": "A very nice Item",
+                    "price": 35.4,
+                    "tax": 3.2,
+                },
+            },
+            "converted": {
+                "summary": "An example with converted data",
+                "description": "FastAPI can convert price `strings` to actual `numbers` automatically",
+                "value": {
+                    "name": "Bar",
+                    "price": "35.4",
+                },
+            },
+            "invalid": {
+                "summary": "Invalid data is rejected with an error",
+                "value": {
+                    "name": "Baz",
+                    "price": "thirty five point four",
+                },
+            },
         },
+        # example={
+        #     "name": "Foo",
+        #     "description": "A very nice Item",
+        #     "price": 35.4,
+        #     "tax": 3.2,
+        # },
     ),
 ):
     results = {"item_id": item_id, "item": item}
