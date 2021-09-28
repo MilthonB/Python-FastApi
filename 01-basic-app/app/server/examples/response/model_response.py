7039cc624dd641b2545804d2118a97f7ee4ba8f3
MB <milthonbor@gmail.com>
2021-09-28 02:01:17 -0700

    unset true

--- 01-basic-app/app/server/examples/response/model_response.py
+++ 01-basic-app/app/server/examples/response/model_response.py
@@ -5,13 +5,13 @@ from pydantic import BaseModel, EmailStr
 
 app = FastAPI()
 
-class UserIn(BaseModel):
+class UserIn(BaseModel):#modelo de entrada
     username: str
     password: str
     email: EmailStr
     full_name: Optional[str] = None
 
-class UserOut(BaseModel):
+class UserOut(BaseModel): # modelo de salida, se omite la contraseña
     username: str
     email: EmailStr
     full_name: Optional[str] = None
@@ -20,3 +20,24 @@ class UserOut(BaseModel):
 @app.post("/user/", response_model=UserOut)
 async def create_user(user: UserIn):
     return user
+
+
+# Parámetros de codificación del modelo de respuesta
+class Item(BaseModel):
+    name: str
+    description: Optional[str] = None
+    price: float
+    tax: float = 10.5
+    tags: List[str] = []
+
+
+items = {
+    "foo": {"name": "Foo", "price": 50.2},#valores predeterminados no se incluirán en la respuesta
+    "bar": {"name": "Bar", "description": "The bartenders", "price": 62, "tax": 20.2}, #valores predeterminados
+    "baz": {"name": "Baz", "description": None, "price": 50.2, "tax": 10.5, "tags": []},#Datos con los mismos valores que los predeterminados
+}
+
+
+@app.get("/exclude/items/{item_id}", response_model=Item, response_model_exclude_unset=True)
+async def read_item(item_id: str):
+    return items[item_id]
