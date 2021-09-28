6956cc6315ab867e2e1ec8794a5c5a6567b420a5
MB <milthonbor@gmail.com>
2021-09-27 20:52:47 -0700

    Tipos especiales y validación

--- 01-basic-app/app/server/examples/body/body_modelos_anidados.py
+++ 01-basic-app/app/server/examples/body/body_modelos_anidados.py
@@ -1,11 +1,22 @@
-from typing import List,Optional,Set
+from typing import List, Optional, Set
 
 from fastapi import FastAPI
-from pydantic import BaseModel
+from pydantic import BaseModel, HttpUrl
 
 app = FastAPI()
 
+# Python tiene un tipo de datos especial para conjuntos de elementos únicos, el set. No alamcena valores duplicados
+# Con esto, incluso si recibe una solicitud con datos duplicados, se convertirá en un conjunto de elementos únicos.
 
+
+# Modelo que se utilizara como submodulo
+class Image(BaseModel):
+    # url: str
+    url: HttpUrl
+    name: str
+
+# Se puede declarar "objetos" JSON profundamente anidados con nombres, tipos y validaciones de atributos específicos.
+# Como image: Optional[Image] = None
 class Item1(BaseModel):
     name: str
     description: Optional[str] = None
@@ -13,6 +24,7 @@ class Item1(BaseModel):
     tax: Optional[float] = None
     # tags: list = [] # No se define el tipo de los elementos
     tags: Set[str] = set()
+    image: Optional[Image] = None
 
 
 class Item2(BaseModel):
@@ -20,10 +32,11 @@ class Item2(BaseModel):
     description: Optional[str] = None
     price: float
     tax: Optional[float] = None
-    tags: List[str] = [] #Especificar los tipos de cada elemento agrefafo a la lista
+    # Especificar los tipos de cada elemento agrefafo a la lista
+    tags: List[str] = []
 
 
-@app.put("/items/{item_id}")
-async def update_item(item_id: int, item: Item2):
+@app.put("/anidados/items/{item_id}")
+async def update_item(item_id: int, item: Item1):
     results = {"item_id": item_id, "item": item}
     return results
