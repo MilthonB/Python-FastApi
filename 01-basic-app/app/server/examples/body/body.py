ee83bc139dcfb7f718b1922459d0b61f4aa88380
MB <milthonbor@gmail.com>
2021-09-27 19:07:31 -0700

    multiples parametros en el body

--- 01-basic-app/app/server/examples/body/body.py
+++ 01-basic-app/app/server/examples/body/body.py
@@ -6,11 +6,14 @@ from pydantic import BaseModel
 app = FastAPI()
 
 
+# creacion del modelo de la peticion put item
 class Item(BaseModel):
     name: str
     description: Optional[str] = None
     price: float
     tax: Optional[float] = None
+
+# Mix Path, Queryy parámetros corporales
 
 
 @app.put("/items/{item_id}")
@@ -26,3 +29,31 @@ async def update_item(
     if item:
         results.update({"item": item})
     return results
+
+
+
+class User(BaseModel):
+    username: str
+    full_name: Optional[str] = None
+
+#multiples parametrso de body: Item y User
+@app.put("/user/items/{item_id}")
+async def update_item(item_id: int, item: Item, user: User):
+    results = {"item_id": item_id, "item": item, "user": user}
+    return results
+
+"""
+Respuesta
+{
+    "item": {
+        "name": "Foo",
+        "description": "The pretender",
+        "price": 42.0,
+        "tax": 3.2
+    },
+    "user": {
+        "username": "dave",
+        "full_name": "Dave Grohl"
+    }
+}"""
+
