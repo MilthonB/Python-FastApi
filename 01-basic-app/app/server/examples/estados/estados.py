4b56265eb6d16cbfd758046997704c6f5e65ec0d
MB <milthonbor@gmail.com>
2021-09-28 18:36:26 -0700

    statys de fastapi

--- 01-basic-app/app/server/examples/estados/estados.py
+++ 01-basic-app/app/server/examples/estados/estados.py
@@ -1,8 +1,9 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, status
 
 app = FastAPI()
 
 
-@app.post("/estados/items/", status_code=200)
+# @app.post("/estados/items/", status_code=200)
+@app.post("/estados/items/", status_code=status.HTTP_201_CREATED)
 async def create_item(name: str):
     return {"name": name}
