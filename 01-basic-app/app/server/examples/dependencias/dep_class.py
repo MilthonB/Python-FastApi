633342eeb1893d06816a151ba8aaa9181a482686
MB <milthonbor@gmail.com>
2021-09-29 14:45:45 -0700

    dependencia invocable

---
+++ 01-basic-app/app/server/examples/dependencias/dep_class.py
@@ -0,0 +1,36 @@
+from typing import Optional
+
+from fastapi import Depends, FastAPI
+
+app = FastAPI()
+
+
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+
+
+"""
+
+Es mucho más facíl devolver un invocable a un dict (diccionario), debido a que el diccionaio no puedes acceder a sus propiedades a menos que 
+sepas o conozcas su llave (key)
+
+Un clase es un invocable en python y ese invocable puede tener parametros o más funciones que pueda ayudar a cumplir o satisfacer la dependencia 
+
+"""
+
+class CommonQueryParams:
+    def __init__(self, q: Optional[str] = None, skip: int = 0, limit: int = 100):
+        self.q = q
+        self.skip = skip
+        self.limit = limit
+
+
+@app.get("/items/")
+
+async def read_items(commons: CommonQueryParams = Depends()):
+
+    response = {}
+    if commons.q:
+        response.update({"q": commons.q})
+    items = fake_items_db[commons.skip : commons.skip + commons.limit]
+    response.update({"items": items})
+    return response
