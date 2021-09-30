e4f8f24e0616c28f0dffe3374f73a8dac1729421
MB <milthonbor@gmail.com>
2021-09-29 18:09:52 -0700

    seguridad sin jwt

--- 02-seguridad/seguridad.py
+++ 02-seguridad/seguridad.py
@@ -1,10 +1,28 @@
 # https://fastapi.tiangolo.com/es/tutorial/security/first-steps/
 from fastapi import Depends, FastAPI
+from pydantic import BaseModel
+from typing import Optional
 from fastapi.security import OAuth2PasswordBearer
 
 app = FastAPI()
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token") #Es una url relativa ./token http://localhost:8080/token
+
+#Creacion de modelo de usuarios
+class User(BaseModel):
+    username: str
+    email: Optional[str] = None
+    full_name: Optional[str] = None
+    disabled: Optional[bool] = None
+
+def fake_decode_token(token):
+    return User(
+        username=token + "fakedecoded", email="john@example.com", full_name="John Doe"
+    )
+
+async def get_current_user(token: str = Depends(oauth2_scheme)):
+    user = fake_decode_token(token)
+    return user
 
 
 @app.get("/items/")
