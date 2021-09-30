88ee5537c035bc98190800455d56a85f8e71a3bb
MB <milthonbor@gmail.com>
2021-09-29 19:56:16 -0700

    db files

---
+++ 01-basic-app/app/server/examples/cors/cors.py
@@ -0,0 +1,43 @@
+from fastapi import FastAPI
+
+from fastapi.middleware.cors import CORSMiddleware
+
+
+app = FastAPI()
+
+# https://fastapi.tiangolo.com/es/tutorial/cors/
+
+
+origins = [
+
+    "http://localhost.tiangolo.com",
+
+    "https://localhost.tiangolo.com",
+
+    "http://localhost",
+
+    "http://localhost:8080",
+
+]
+
+
+
+app.add_middleware(
+
+    CORSMiddleware,
+
+    allow_origins=origins,
+
+    allow_credentials=True,
+
+    allow_methods=["*"],
+
+    allow_headers=["*"],
+
+)
+
+
+
+@app.get("/")
+async def main():
+    return {"message": "Hello World"}
