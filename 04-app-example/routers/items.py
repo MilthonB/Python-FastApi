9676bbf48f85908f8da5c100c2743d203f58a173
MB <milthonbor@gmail.com>
2021-10-01 18:21:44 -0700

    limpiza

--- 04-app-example/routers/items.py
+++ 04-app-example/routers/items.py
@@ -1,49 +1,43 @@
 from fastapi import APIRouter, Depends, HTTPException
-
 from ..dependencies import get_token_header
 
-
 router = APIRouter(
-
     prefix="/items",
-
     tags=["items"],
-
     dependencies=[Depends(get_token_header)],
-
-    responses={404: {"description": "Not found"}},
-
+    responses={404: {"Ejemplo": "Error"}},
 )
-
-
 
 fake_items_db = {"plumbus": {"name": "Plumbus"}, "gun": {"name": "Portal Gun"}}
 
 
-
 @router.get("/")
-
 async def read_items():
     return fake_items_db
 
 
-
 @router.get("/{item_id}")
-
 async def read_item(item_id: str):
     if item_id not in fake_items_db:
-        raise HTTPException(status_code=404, detail="Item not found")
+        raise HTTPException(status_code=404, detail={
+            'ok': False,
+            'msg': 'El item no se encontro',
+            'item_id': item_id
+        })
     return {"name": fake_items_db[item_id]["name"], "item_id": item_id}
-
 
 @router.put(
     "/{item_id}",
     tags=["custom"],
-    responses={403: {"description": "Operation forbidden"}},
+    responses={403: {"description": "Operation forbidden 123"}},
 )
 async def update_item(item_id: str):
     if item_id != "plumbus":
         raise HTTPException(
-            status_code=403, detail="You can only update the item: plumbus"
+            status_code=403, detail={
+                'ok':False,
+                'msg':'Sólo puedes actualizar el elemento plumbus',
+                'item_id': item_id
+            }
         )
     return {"item_id": item_id, "name": "The great Plumbus"}
