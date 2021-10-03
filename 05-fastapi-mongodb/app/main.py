de4ecfa9ffb7fc65371cd296b8473c0a958b1085
MB <milthonbor@gmail.com>
2021-10-02 21:34:45 -0700

    get put

--- 05-fastapi-mongodb/app/main.py
+++ 05-fastapi-mongodb/app/main.py
@@ -1,7 +1,9 @@
 
 from typing import Optional, List
+from bson.py3compat import b
 from fastapi import FastAPI
 from fastapi.param_functions import Body, Header, Query
+from fastapi.params import Param
 from pydantic import BaseModel, EmailStr
 from bson import ObjectId
 from pydantic.fields import Field
@@ -19,53 +21,56 @@ app = FastAPI(
 db = Conexion()
 
 # Esto no se para que sirve aúns
-# class PyObjectId(ObjectId):
+class PyObjectId(ObjectId):
     
-#     @classmethod
-#     def __get_validators__(cls):
-#         yield cls.validate
+    @classmethod
+    def __get_validators__(cls):
+        yield cls.validate
 
-#     @classmethod
-#     def validate(cls, v):
-#         if not ObjectId.is_valid(v):
-#             raise ValueError('Invalid objectid')
-#         return ObjectId(v)
+    @classmethod
+    def validate(cls, v):
+        if not ObjectId.is_valid(v):
+            raise ValueError('Invalid objectid')
+        return ObjectId(v)
 
-#     @classmethod
-#     def __modify_schema__(cls, field_schema):
-#         field_schema.update(type='string')
-
-
+    @classmethod
+    def __modify_schema__(cls, field_schema):
+        field_schema.update(type='string')
 
 
 class DB(BaseModel): # Documento para pasar a la base de datos 
-    id: int
     nombre:str
     email: EmailStr
     activo: Optional[bool] = True 
 
-   
 
+class DB_out(DB): # Documento para pasar a la base de datos 
+    id: Optional[PyObjectId] = Field(alias='_id')
 
+    class Config:
+        arbitrary_types_allowed = True
+        json_encoders = {
+            ObjectId: str
+        }
 
-@app.post('/db/{db_id}', response_model=DB)
-async def obtener_data(q:int = Query(...), x_token: str = Header(...), body:DB = Body(..., embed=True)) -> List:
-    print('no hay error', body.dict())
-   
-    # if hasattr(body, 'id'):
-    #     delattr(body, 'id')
-   
-    print(body)
-
-    dictAux = {
-        'nada':'nada'
-    }
-
-    collection = db.coleccion_alumnos# asi se crean colecciones
-    # post = db.posts#coleccion
+@app.post('/post/{db_id}', response_model=DB_out)
+async def insertar_data(q:int = Query(...), x_token: str = Header(...), body:DB = Body(..., embed=True)) -> List:
+    collection = db.coleccion_alumnos #obtener la colleccion que se va a utilizar
     #insert document
-    id = collection.insert_one(dictAux).inserted_id #Insert en la coleccion y devuelve el id
-    print(id) #Imprime el id
-    # print (db.list_collection_names())
-    return body #retorna el body recibivo
+    id = collection.insert_one(body.dict()).inserted_id #Insert en la coleccion y devuelve el id
+    response = DB_out(_id = id, **body.dict())
+    return response
 
+@app.get('/get/{alumno_id}',response_model=DB_out)
+async def obtener_data(alumno_id:str = Param(...)):
+    alumnos = db.coleccion_alumnos.find_one({'_id': ObjectId(alumno_id)})
+    return alumnos
+
+
+@app.put('/put/{alumno_id}')
+async def actualizar_data():
+    ...
+
+@app.delete('/delete/{alumno_id}')
+async def actualizar_data():
+    ...
