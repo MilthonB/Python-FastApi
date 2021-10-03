89cb0851e0743ac0495c48edaf1745c3e42a30ec
MB <milthonbor@gmail.com>
2021-10-02 20:48:18 -0700

    config

--- 05-fastapi-mongodb/app/dbmongo/config.py
+++ 05-fastapi-mongodb/app/dbmongo/config.py
@@ -2,4 +2,24 @@
 
 from pymongo import MongoClient
 
-client = MongoClient()
+from pymongo.collection import Collection 
+
+
+class Conexion(object):
+
+    def __init__(self):
+        self.__uri = f'mongodb+srv://fastApi:XUtKn6tvRlO1vyuj@clusterfastapi.ppox7.mongodb.net/test?authSource=admin&replicaSet=atlas-vvtkil-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true'
+        self.__client = MongoClient(self.__uri)
+        self.__data_base = self.__client.fastApi
+
+    @property
+    def coleccion_alumnos(self):
+        return self.__data_base.alumnos
+
+    @property
+    def coleccion_semestre(self):
+        return self.__data_base.semestre
+    
+    @property
+    def coleccion_carrera(self):
+        return self.__data_base.carrera
--- 05-fastapi-mongodb/app/main.py
+++ 05-fastapi-mongodb/app/main.py
@@ -4,8 +4,9 @@ from fastapi import FastAPI
 from fastapi.param_functions import Body, Header, Query
 from pydantic import BaseModel, EmailStr
 from bson import ObjectId
+from pydantic.fields import Field
 
-from dbmongo.config import client
+from dbmongo.config import Conexion
 
 
 
@@ -15,24 +16,56 @@ app = FastAPI(
     # dependencies= [Depends(...)]
 );
 
-class DB(BaseModel):
-    id:str
+db = Conexion()
+
+# Esto no se para que sirve aúns
+# class PyObjectId(ObjectId):
+    
+#     @classmethod
+#     def __get_validators__(cls):
+#         yield cls.validate
+
+#     @classmethod
+#     def validate(cls, v):
+#         if not ObjectId.is_valid(v):
+#             raise ValueError('Invalid objectid')
+#         return ObjectId(v)
+
+#     @classmethod
+#     def __modify_schema__(cls, field_schema):
+#         field_schema.update(type='string')
+
+
+
+
+class DB(BaseModel): # Documento para pasar a la base de datos 
+    id: int
     nombre:str
     email: EmailStr
     activo: Optional[bool] = True 
 
+   
 
-print(client)
 
 
 @app.post('/db/{db_id}', response_model=DB)
 async def obtener_data(q:int = Query(...), x_token: str = Header(...), body:DB = Body(..., embed=True)) -> List:
     print('no hay error', body.dict())
-    db = client.test_database
-    # coleccion = db['test-collection']
-    # print(coleccion)
-    post = db.posts
-    id = post.insert_one(body.dict()).inserted_id
-    print(id)
-    return body
+   
+    # if hasattr(body, 'id'):
+    #     delattr(body, 'id')
+   
+    print(body)
+
+    dictAux = {
+        'nada':'nada'
+    }
+
+    collection = db.coleccion_alumnos# asi se crean colecciones
+    # post = db.posts#coleccion
+    #insert document
+    id = collection.insert_one(dictAux).inserted_id #Insert en la coleccion y devuelve el id
+    print(id) #Imprime el id
+    # print (db.list_collection_names())
+    return body #retorna el body recibivo
 
