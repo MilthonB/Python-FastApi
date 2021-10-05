9d10962e7955e155e89bbc078a74d2cabe0c841c
MB <milthonbor@gmail.com>
2021-10-04 17:41:04 -0700

    str mongo id

--- 06-fastApi-cafe/app/controllers/uploads_img.py
+++ 06-fastApi-cafe/app/controllers/uploads_img.py
@@ -1,21 +1,24 @@
 
 
 from fastapi.param_functions import File
+import cloudinary
+
+
 
 
 class Img(object):
 
-    def get_img(self, id_usuario:int):
+    def get_img(self, id:str):
         return {
             'img':'Se envío la img'
         }
     
-    def update_img(self, coleccion: str, id_usuario: str):
+    def update_img(self, coleccion: str, id: str):
         return {
             'img':'Se actualizó la img'
         }
 
-    def post_img(self, coleccion: str, id_usuario: str, file: File):
+    def post_img(self, coleccion: str, id: str, file: File):
         #Mandar la url de la foto almacenada en la file de la root
         return {
             'img':'Se creó una img'
