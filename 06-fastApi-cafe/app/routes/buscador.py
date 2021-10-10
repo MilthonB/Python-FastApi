2f2309f06eb3ba43c5f05d88f06f3868ab734945
MB <milthonbor@gmail.com>
2021-10-10 00:22:19 -0700

    dependencias buscaores  toke jwt

--- 06-fastApi-cafe/app/routes/buscador.py
+++ 06-fastApi-cafe/app/routes/buscador.py
@@ -1,12 +1,14 @@
 
-from fastapi import APIRouter, Path
+from fastapi import APIRouter, Path, Header, Depends
 from controllers.buscador import Buscador
+from helpers.dependencia_jwt import jwt_decode
+
 
 router = APIRouter( tags=['Buscador'])
 
 buscar = Buscador()
 
-@router.get('/search/{coleccion}/{termino}')
-async def buscado(coleccion:str = Path(...), termino:str = Path(...)):
+@router.get('/search/{coleccion}/{termino}', dependencies=[Depends(jwt_decode)])
+async def buscado(x_token:str = Header(..., convert_underscores=False), coleccion:str = Path(...), termino:str = Path(...)):
     resp = await buscar.buscador(termino, coleccion)
     return resp
